c70503415dc7a4b902cc40296153cfdb4fb6609c
Melimermazing <jidapa.melisa@gmail.com>
2023-04-27 15:47:55 +0000

    Home  - Size and Adjust Grid

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,33 +1,54 @@
 import"./Home.css";
 import Header from "../../components/Header/Header";import { styled } from '@mui/material/styles';
 import {Box, Grid} from '@mui/material';
+import FoodBasket from "../../assets/Food_Basket.svg"
 
 
 const Home = () => {
   return (
     <div>
       <Header/>
-      <h1>Home</h1>
 
-      <Grid container spacing={2}>
-       <Grid item xs={6} md={8}>
+      {/* ---> This is main grid <--- */}
+      <Grid container spacing={2} sx={{ width: '100%', margin: '0 auto', maxWidth: '1800px' }}>
+       <Grid className="MainGrid" item xs={6} md={8}>
           <Box bgcolor="grey" p={10}>
-            This is Main Grid
-          </Box>
-        </Grid>
-        <Grid item xs={6} md={4}>
-          <Box bgcolor="grey" p={10}>
-            This is sidebar box 
-            <Box bgcolor="brown" p={10}>
-            This is box 1 inside sidebar
+
+            <Box bgcolor="brown" p={10} style={{ marginBottom: '20px' }}>
+            <img src={FoodBasket} alt="Food Basket" />
+            <h2>This is Main Box</h2> 
+            <p>Content</p>
             </Box>
 
-            <Box bgcolor="white" p={10}>
-            This is box 2 inside sidebar
+          </Box>
+        </Grid>
+
+        {/* ---> This is side grid <--- */}
+        <Grid className="SideGrid" item xs={6} md={4} sx={{ height: '100%' }}>
+          <Box bgcolor="grey" p={10} sx={{ height: '100%', paddingLeft: 10 }}>
+            <h1>Trending</h1>
+            <Box bgcolor="brown" p={10} style={{ marginBottom: '20px' }}>
+            <img src={FoodBasket} alt="Food Basket" style={{ width: '200px', height: '200px', float: 'left', marginRight: '10px' }} />
+            <div style={{ display: 'inline-block' }}>
+              <h2>This is box 1</h2> 
+              <p>Content</p>
+            </div>
+            </Box>
+
+            <Box bgcolor="white" p={10} style={{ marginBottom: '20px' }}>
+            <img src={FoodBasket} alt="Food Basket" style={{ width: '200px', height: '200px', float: 'left', marginRight: '10px' }} />
+            <div style={{ display: 'inline-block' }}>
+            <h3>This is box 2</h3> 
+            <p>Content</p>
+            </div>
             </Box>
 
             <Box bgcolor="pink" p={10} >
-            This is box 3 inside sidebar
+            <img src={FoodBasket} alt="Food Basket" style={{ width: '200px', height: '200px', float: 'left', marginRight: '10px' }} />
+            <div style={{ display: 'inline-block' }}>
+            <h3>This is box 3</h3> 
+            <p>Content</p>
+            </div>
             </Box>
 
           </Box>
