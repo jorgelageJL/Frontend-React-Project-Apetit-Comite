65675c3ff33e75dd8eac645a79144558f093300a
Melimermazing <jidapa.melisa@gmail.com>
2023-04-27 14:14:22 +0000

    Homepage  - Add Grid

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,37 @@
 import"./Home.css";
-import Header from "../../components/Header/Header";
+import Header from "../../components/Header/Header";import { styled } from '@mui/material/styles';
+import {Box, Grid} from '@mui/material';
 
 const Home = () => {
   return (
     <div>
       <Header/>
       <h1>Home</h1>
+
+      <Grid container spacing={2}>
+       <Grid item xs={6} md={8}>
+          <Box bgcolor="grey" p={10}>
+            This is Main Grid
+          </Box>
+        </Grid>
+        <Grid item xs={6} md={4}>
+          <Box bgcolor="grey" p={10}>
+            This is sidebar box 
+            <Box bgcolor="brown" p={10}>
+            This is box 1 inside sidebar
+            </Box>
+
+            <Box bgcolor="white" p={10}>
+            This is box 2 inside sidebar
+            </Box>
+
+            <Box bgcolor="pink" p={10} >
+            This is box 3 inside sidebar
+            </Box>
+
+          </Box>
+        </Grid>
+        </Grid>
     </div>
   );
 };
