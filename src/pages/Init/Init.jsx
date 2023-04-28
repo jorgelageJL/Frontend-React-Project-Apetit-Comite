41aa2a7d5c0eb705a514c798ffe6f68ace7d3f02
abian22 <abian.camejo@gmail.com>
2023-04-28 10:30:46 +0100

    seguir con el logo y sign in

--- src/pages/Init/Init.jsx
+++ src/pages/Init/Init.jsx
@@ -1,12 +1,27 @@
-
 import LoginHeader from "../../components/LoginHeader/LoginHeader";
+import logo from "../../assets/Full_Logo.png"
+import { Button } from "@mui/material";
+import "./init.css";
 
 const Init = () => {
+
   return (
-    <div>
+    <>
       <LoginHeader />
-      <h1>asdqwe</h1>
-    </div>
+      <div className="fondo">
+        <div className="=container">asdqweasd</div>
+        <img className="logo" src={logo} />
+        <div className="signin">
+          <Button sx={{ my: 2, color: "black", display: "inline" }}>
+            <b>Sign up</b>
+          </Button>
+          |
+          <Button sx={{ my: 2, color: "black", display: "inline" }}>
+            <b>Log in</b>
+          </Button>
+        </div>
+      </div>
+    </>
   );
 };
 
