819d933b8f92b40cb50a3e235d17c695d9c60e4f
jorgelageJL <jorgeluis.lage@yahoo.com>
2023-05-09 10:17:28 +0100

    onKey event in Loggin and SingUp added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,16 +1,26 @@
 import { useState } from "react";
 import { Email, Lock, Visibility, VisibilityOff } from "@mui/icons-material";
-import { Button, Card, CardActions, CardContent, CardHeader, Divider, Grid, IconButton, TextField, } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Divider,
+  Grid,
+  IconButton,
+  TextField,
+} from "@mui/material";
 import { blue } from "@mui/material/colors";
-import { login } from '../../Services/auth'
+import { login } from "../../Services/auth";
 import { useNavigate } from "react-router-dom";
-import './Login.css'
+import "./Login.css";
 
 export default function Login() {
   const [isPassVisible, setIsPassVisible] = useState(false);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const navigate = useNavigate()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   function handleClick() {
     setIsPassVisible(!isPassVisible);
@@ -19,14 +29,14 @@ export default function Login() {
   async function onLogin() {
     try {
       if (email && password) {
-        const loginResponse = await login({email, password})
-        localStorage.setItem('token', loginResponse.data.token)
-        navigate('/home')
+        const loginResponse = await login({ email, password });
+        localStorage.setItem("token", loginResponse.data.token);
+        navigate("/home");
       } else {
-        throw new Error()
+        throw new Error();
       }
     } catch (error) {
-      alert('Email or Password invalid')
+      alert("Email or Password invalid");
     }
   }
 
@@ -47,6 +57,7 @@ export default function Login() {
               startAdornment: <Email />,
             }}
             onChange={(e) => setEmail(e.target.value)}
+            onKeyDown={(e) => {if (e.key === 'Enter') onLogin()}}
           ></TextField>
 
           <TextField
@@ -64,6 +75,7 @@ export default function Login() {
               ),
             }}
             onChange={(e) => setPassword(e.target.value)}
+            onKeyDown={(e) => {if (e.key === 'Enter') onLogin()}}
           ></TextField>
         </CardContent>
 
@@ -71,17 +83,21 @@ export default function Login() {
 
         <CardActions sx={{ display: "flex", justifyContent: "flex-end" }}>
           <Button
-            onClick={() => navigate('/signUp')}
+            onClick={() => navigate("/signUp")}
             size="small"
             color="secondary"
             variant="contained"
-            sx={{ marginRight: "1vw" }}>
+            sx={{ marginRight: "1vw" }}
+          >
             Register
           </Button>
 
           <Button
             onClick={() => onLogin()}
-            size="small" color="primary" variant="contained">
+            size="small"
+            color="primary"
+            variant="contained"
+          >
             Login
           </Button>
         </CardActions>
@@ -90,9 +106,17 @@ export default function Login() {
   }
 
   return (
-      <Grid container /*item xs={12} md={8} xl={10}*/
-        sx={{ height: '85vh', display: "flex", flexDirection: 'column', justifyContent: "center", alignContent: 'center' }}>
-        {render()}
-      </Grid>
+    <Grid
+      container /*item xs={12} md={8} xl={10}*/
+      sx={{
+        height: "85vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignContent: "center",
+      }}
+    >
+      {render()}
+    </Grid>
   );
 }
