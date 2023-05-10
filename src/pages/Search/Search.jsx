9ac89ab3207b328ed66fba478c8d73c43a1af02a
Abián <abian.camejo@gmail.com>
2023-05-10 22:32:22 +0100

    search finished

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -2,22 +2,25 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import './Search.css'
 import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Grid, Typography } from '@mui/material';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { getOneRecipeByName } from '../../Services/recipeServices';
 import { Link as RouterLink } from 'react-router-dom';
 import './Search.css'
 
 export default function Search() {
   const [text, setText] = useState('')
-  const [recipe, setRecipe] = useState({})
+  const [recipes, setRecipes] = useState([])
+
+  useEffect(() => {
+    handleSearch
+  }, [])
 
   async function handleSearch() {
     if (text) {
-      const result = await getOneRecipeByName(text)
-      setRecipe(result)
-    }
+      const results = await getOneRecipeByName(text)
+      setRecipes(results)
+    } 
   } 
-
 
   return (
     <>
@@ -57,41 +60,45 @@ export default function Search() {
        
       </Box>
 
-      {/* {CARD} */}
-      <Grid
-        item
-        xs={12}
-        sm={6}
-        md={4}
-        xl={3}
-        border="solid"
-        alignContent="center"
-        display="flex"
-        justifyContent="center"
-      >
-        <Card
-          sx={{
-            maxWidth: "345px",
-            padding: "10px",
-            margin: "10px",
-            flexDirection: "row",
-            minWidth: "345px",
-          }}
-        >
-          <CardActionArea component={RouterLink} to={`${recipe.id}`}>
-            <CardMedia component="img" height="140" image={recipe.img} />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {recipe.name}
-              </Typography>
-              <Typography variant="body2" sx={{ color: "black" }}>
-                {recipe.description}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
+      {/* {CARDS} */}
+      <Grid container spacing={3}>
+        {recipes.map((recipe) => (
+          <Grid
+            key={recipe.id}
+            item
+            xs={12}
+            sm={6}
+            md={4}
+            xl={3}
+            alignContent="center"
+            display="flex"
+            justifyContent="center"
+          >
+            <Card
+              sx={{
+                maxWidth: "345px",
+                padding: "10px",
+                margin: "10px",
+                flexDirection: "row",
+                minWidth: "345px",
+              }}
+            >
+              <CardActionArea component={RouterLink} to={`${recipe.id}`}>
+                <CardMedia component="img" height="140" image={recipe.img} />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {recipe.name}
+                  </Typography>
+                  <Typography variant="body2" sx={{ color: "black" }}>
+                    {recipe.description}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
 
-          <CardActions></CardActions>
-        </Card>
+              <CardActions></CardActions>
+            </Card>
+          </Grid>
+        ))}
       </Grid>
     </>
   );
